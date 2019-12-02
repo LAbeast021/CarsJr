6dc532d65c74afcb2aa91d2d2fd96e21a1045430
LAbeast021 <kiarashhesampour@gmail.com>
2019-12-01 20:51:44 -0800

    router

--- config/passport.js
+++ config/passport.js
@@ -3,8 +3,9 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_SECRET,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: process.env.GOOGLE_CALLBACK
 },function(accessToken, refreshToken, profile, cb){
     console.log(profile);
-}))
+    return cb(null);
+}));

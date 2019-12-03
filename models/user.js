386ae90a4e0d83f6b3159b1e789f9ddf7feb1840
LAbeast021 <kiarashhesampour@gmail.com>
2019-12-03 09:11:03 -0800

    added caption and caption

--- models/user.js
+++ models/user.js
@@ -1,11 +1,30 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var commentSchema = new Schema ({
+    comment:String
+},{
+    timestamps:true
+})
+
+var postSchema = new Schema({
+    image: String,
+    brand:String,
+    model: String,
+    year: Date,
+    horsePower: Number,
+    caption: String,
+    comments:[commentSchema]
+
+},{
+    timestamps:true
+})
 var userSchema = new Schema ({
     name:String,
     email:String,
     avatar:String,
-    googleId:String
+    googleId:String,
+    posts:[postSchema]
 },{
     timestamps:true
 });

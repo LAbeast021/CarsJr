f7a8f9c0173607cc29085a53254bc68420de4055
LAbeast021 <kiarashhesampour@gmail.com>
2019-12-06 00:29:06 -0800

    final touches

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user')
 var multer = require('multer');
 var cloudinary = require('cloudinary');
 var cloudinaryStorage = require('multer-storage-cloudinary');
@@ -19,6 +20,24 @@ router.post('/comment/:id' , postsCtrl.createComment);
 router.delete('/comment/:id', postsCtrl.deleteComment)
 router.post('/upload', upload.single('image') , postsCtrl.createPost)
 router.get('/new',isLoggedIn ,postsCtrl.newPost);
+// ////////////////////////////////////////////////////////////////////////////
+router.get('/edit/:id',isLoggedIn ,function(req,res){
+  User.findOne({_id:req.user._id},function(err, user){
+    var value = user.posts[req.params.id].caption
+    res.render('users/edit-caption',{
+      idx:req.params.id,
+      value
+    })
+  })
+})
+router.post('/edit/:id', function(req,res){
+  User.findOne({_id:req.user._id},function(err, user){
+    user.posts[req.params.id].caption = req.body.caption
+    user.save(function(err){
+      res.redirect(`/users/profile/${req.user._id}`)
+    })
+  })
+})
 
 
 function isLoggedIn(req, res, next) {

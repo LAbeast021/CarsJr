39649c35e81ce9242d6e4fbc124c7ce8583e52a7
LAbeast021 <kiarashhesampour@gmail.com>
2020-02-11 20:06:17 -0800

    likes

--- routes/home.js
+++ routes/home.js
@@ -4,6 +4,26 @@ var router = express.Router();
 var User = require('../models/user');
 var Post = require('../models/post');
 
+
+
+
+////////////////////// LIKES ////////////////////////////////////////////////
+router.get('/likes/:uId/:pId',function(req,res){
+  Post.findById(req.params.pId,function(err,post){
+    if(post.likes.includes(req.params.uId)){
+      post.likes.remove(req.params.uId)
+    }
+    else{
+      post.likes.push(req.params.uId)
+    }
+    post.save(function(err){
+      res.status(200).json(post.likes);
+    })
+  })
+  
+})
+// ///////////////////////////////////////////////////////////////////////////
+
 router.get('/home',isLoggedIn,async function(req,res){
   let posts = await Post.find({ $query: {}, $orderby: { createdAt : -1 } })
   // Post.find({}).sort({'created_at': -1})(function(err,posts){

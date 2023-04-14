5bfbe06e6fd580b67a85b5e3871cd8e48ac2b95c
LAbeast021 <kiarashhesampour@gmail.com>
2023-04-14 16:41:51 -0700

    bug fixed , mongoose new version doesnt need parameters

--- config/database.js
+++ config/database.js
@@ -10,3 +10,10 @@ var db = mongoose.connection;
 db.on('connected', function (err) {
   console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
 });
+
+// NEW VERSION OF MONGOOSE ALREADY ASSUMES ALL THE FOLLOWIN  }}}}} IF THERE IS AN ERROR CONNECTING TO DATABASE {{{{{{{{{}}}}}}}}}
+  // useNewUrlParser , useUnifiedTopology , useFindAndModify , and useCreateIndex are no longer supported options
+  // . Mongoose 6 always behaves as if useNewUrlParser , useUnifiedTopology , and useCreateIndex are true , and useFindAndModify is false .
+  // useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useUnifiedTopology: true
